f0184537b8867ab3dcb729580333d290ef91f9be
saguit03 <saraguillentorrado@gmail.com>
2025-03-03 13:02:00 +0100

    Fix 0.3

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,19 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.48.0"
     }
   }
 }
 
+provider "aws" {
+  
+}
+
 resource "aws_instance" "ec2" {
-  ami           = "ami-0c02fb55956c7d316"  # AMI de Amazon Linux 2
-  instance_type = "t2.micro"               # Tipo de instancia
+  ami           = "ami-0c02fb55956c7d316" # AMI de Amazon Linux 2
+  instance_type = "t2.micro"              # Tipo de instancia
 
   tags = {
     Name = "MiInstanciaEC2"
